80840ef6bcfb3cb71cf93626af077d34f7b96362
krishkamani <kdkamani00@gmail.com>
2021-10-10 12:24:03 +0530

    User Management CRUD Created

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+
+$('#add_user').submit(function(event){
+    toastr.success('Data Inserted Successfully!');
+});
+
+$('#update_user').submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    });
+
+    var request = {
+        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        toastr.success('Data Updated Successfully!');
+    })
+});
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                location.reload();
+                toastr.success('message', 'Data Deleted Successfully!');
+            })
+        }
+
+    })
+}
